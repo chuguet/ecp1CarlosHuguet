f17e217e1be0ba3d62cb0a6d1390f49e628299b9
Carlos Huguet <huguet10@gmail.com>
2014-10-05 12:24:21 +0200

    Carlos Huguet

---
+++ src/main/java/es/upm/miw/spai/HolaMundo.java
@@ -0,0 +1,8 @@
+package es.upm.miw.spai;
+
+public class HolaMundo {
+
+    public static void main(String[] args) {
+        System.out.println("Hola Mundo");
+    }
+}
---
+++ src/test/java/es/upm/miw/spai/HolaMundoTest.java
@@ -0,0 +1,5 @@
+package es.upm.miw.spai;
+
+public class HolaMundoTest {
+
+}
